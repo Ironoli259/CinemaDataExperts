a27093905a55ba475ff32f42f4c9e14cb5367f72
Olivier Grenier <olivier.grenier5@gmail.com>
2023-04-23 17:27:26 -0400

    Fixed error with to combination/integration of code

--- CinemaProject/CinemaReservation.h
+++ CinemaProject/CinemaReservation.h
@@ -4,16 +4,29 @@
 #include "MovieList.h"
 #include "HallsTree.h"
 #include <vector>
+#include <queue>
+
+struct TicketReservation {
+	int hallNumber;
+	int showTimeIndex;
+	int numberOfTickets;
+};
+
 class CinemaReservation
 {
 private:
-public:
-	std::vector<User> userList;
+	std::queue<TicketReservation> ticketReservations;
+	std::vector<User*> userList;
+	User* currentUser;
+public:	
 	void Initialize();
 	void MainMenu();
 	Movie* SearchMovieMenu();
 	CinemaHall* SearchHallsMenu();
 	void ExitApp();
+	std::vector<User*> GetUserList();
+	void SetCurrentUser(User* user);
+	void AddUser(std::string userName);
 	std::string CalculateEndTime(int hours, int minutes, int minsToAdd);
 };
 
